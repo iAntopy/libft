b67ea5bf9a1bec51125dd1b988fea00fb34ddcd7
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2022-09-03 00:28:15 -0400

    fixed minor crashes and norminette on find_file_in_env.c

--- strtab_utils.c
+++ strtab_utils.c
@@ -6,7 +6,7 @@
 /*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 04:23:34 by iamongeo          #+#    #+#             */
-/*   Updated: 2022/08/28 04:37:04 by iamongeo         ###   ########.fr       */
+/*   Updated: 2022/09/02 21:43:12 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ int	strtab_len(char **tab)
 	return (i);
 }
 
-void	strtab_clear(char **tab)
+void	strtab_clear(char ***tab)
 {
 	char	**t;
 
-	t = tab;
+	t = *tab;
 	t--;
 	while (*(++t))
 		malloc_free_p(0, (void **)t);
-	free(tab);
+	malloc_free_p(0, (void **)tab);
 }
 
 void	strtab_swap(char **tab, int index1, int index2)
