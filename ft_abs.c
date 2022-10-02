5ae0563537012841bf02759a3f0ec75369d9b33a
iAntopy <i-antony@hotmail.com>
2022-10-02 19:22:11 -0400

    added, test and norminetted ft_abs for absolute values.

---
+++ ft_abs.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/02 19:12:42 by iamongeo          #+#    #+#             */
+/*   Updated: 2022/10/02 19:21:11 by iamongeo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_abs(int nb)
+{
+	int	mask;
+
+	mask = nb >> 31;
+	return ((nb ^ mask) - mask);
+}
+/*
+int	main()
+{
+	int	a = 15;
+	int	b = -15;
+
+	ft_printf("a, ft_abs(a) : %d, %d\n", a, ft_abs(a));
+	ft_printf("b, ft_abs(b) : %d, %d\n", b, ft_abs(b));
+	return (0);
+}
+*/
